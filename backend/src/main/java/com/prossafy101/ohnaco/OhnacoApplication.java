c9dffbb4e01c8d83a1d20a6f0a889b95a55ecc5a
손창환 <sonch12@naver.com>
2021-07-23 17:31:07 +0900

    프로젝트 생성 /BE
    프로젝트 생성

---
+++ backend/src/main/java/com/prossafy101/ohnaco/OhnacoApplication.java
@@ -0,0 +1,13 @@
+package com.prossafy101.ohnaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhnacoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OhnacoApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/prossafy101/ohnaco/OhnacoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prossafy101.ohnaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhnacoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
