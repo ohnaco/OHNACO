918799dacb69fe988185f6c17e18d7969e4ecc8e
김현홍 <kimho1995@naver.com>
2021-07-26 15:05:20 +0900

    S05P12A101-137 - [feat] : 이메일과 인증코드 레디스에 저장/BE
    이메일과 패스워드 인증코드를 레디스에 저장
    인증코드는 대문자와 숫자조합으로 8개 생성
    
    Resolves S05P12A101-137

--- backend/src/main/java/com/prossafy101/ohnaco/controller/UserController.java
+++ backend/src/main/java/com/prossafy101/ohnaco/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.prossafy101.ohnaco.controller;
 
+import com.prossafy101.ohnaco.entity.user.TempUserDto;
 import com.prossafy101.ohnaco.service.UserService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 @RestController
@@ -26,8 +26,25 @@ public class UserController {
             msg = "false";
         }
         ResponseEntity response = new ResponseEntity<>(msg, HttpStatus.OK);
+
+        //임시로 레디스 저장된 이메일key 삭제!!!!
+        userService.tempUserDelete(email);
         return response;
     }
+
+    @PostMapping("/join")
+    @ApiOperation(value = "임시정보 저장 => email, password값 전달")
+    public Object tempJoin(@RequestBody Map<String, String> tempUser) {
+        String msg = "success";
+        userService.tempUserSave(TempUserDto.builder()
+                .email(tempUser.get("email"))
+                .password(tempUser.get("password"))
+                .token(userService.randomCode())
+                .build());
+        ResponseEntity response = new ResponseEntity<>(msg, HttpStatus.OK);
+        return response;
+    }
+
 
 
 
--- backend/src/main/java/com/prossafy101/ohnaco/service/UserService.java
+++ backend/src/main/java/com/prossafy101/ohnaco/service/UserService.java
@@ -1,20 +1,54 @@
 package com.prossafy101.ohnaco.service;
 
+import com.prossafy101.ohnaco.entity.user.TempUserDto;
+import com.prossafy101.ohnaco.repository.TempUserRepository;
 import com.prossafy101.ohnaco.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepo;
+    @Autowired
+    private TempUserRepository tempUserRepo;
 
+    //중복 이메일 체크
     public boolean isSameEmail(String email) {
         boolean isSameEmail = false;
         if(userRepo.findByEmail(email) != null) {
             isSameEmail = true;
         }
         return isSameEmail;
+    }
+    // 레디스에 이메일(기본키), 패스워드, 인증코드 저장
+    public void tempUserSave(TempUserDto tempUserDto) {
+        tempUserRepo.save(tempUserDto);
+    }
+    //이메일 랜덤 대문자 + 숫자 8개 choice
+    public String randomCode() {
+        StringBuffer sb = new StringBuffer();
+        Random rand = new Random();
+        for(int i=0; i<8; i++) {
+            int randNum = rand.nextInt(2);
+            if(randNum == 0) {
+                sb.append((char)((int)(rand.nextInt(26)+'A')));
+            } else {
+                sb.append(rand.nextInt(10));
+            }
+        }
+        return sb.toString();
+    }
+    //레디스에서 email 키로 삭제
+    public void tempUserDelete(String email) {
+        tempUserRepo.deleteById(email);
+    }
+    //레디스에서 email 키로 찾기
+    public Optional<TempUserDto> tempUserByEmail(String email) {
+        return tempUserRepo.findById(email);
     }
 
 }
