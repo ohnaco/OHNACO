c2cc4788dc04d445f65a8973803ea84182912a21
손창환 <sonch12@naver.com>
2021-07-26 14:33:22 +0900

    S05P12A101-159 - [feat] : JWT 토큰  생성 / 관리 Service 생성 / BE
    - pom.xml에 jwt 라이브러리 추가
    - application.properties에 key에 필요한 String 설정
    - JwtUtil 구현 : JWT 토큰 생성 / 검사
    
    Resolves S05P12A101-159

---
+++ backend/src/main/java/com/prossafy101/ohnaco/service/JwtUtil.java
@@ -0,0 +1,93 @@
+package com.prossafy101.ohnaco.service;
+
+import com.prossafy101.ohnaco.entity.user.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    public final static long TOKEN_VALIDATION_SECOND = 1000L * 10;
+    public final static long REFRESH_TOKEN_VALIDATION_SECOND = 1000L * 60 * 24 * 2;
+
+    public final static String ACCESS_TOKEN_NAME = "accessToken";
+    public final static String REFRESH_TOKEN_NAME = "refreshToken";
+
+    @Value("${spring.jwt.secret}")
+    private String SECRET_KEY;
+
+    /*
+    secretKey 해싱 키로 만들기
+     */
+    private Key getSigningKey(String secretKey) {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    /*
+    JWT Payload에 담는 정보의 한 '조각'을 Claim이라 한다.
+
+    Jwt Parser를 빌드하고 Parser에 토큰 넣어서 payload(body) 부분 발췌
+     */
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey(SECRET_KEY))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+
+    // 발췌한 payload에서 email 추출
+    public String getEmail(String token) {
+        return extractAllClaims(token).get("email", String.class);
+    }
+
+    // 토큰이 만료되었는지 확인
+    public Boolean isTokenExpired(String token) {
+        Date expiration = extractAllClaims(token).getExpiration();
+        return expiration.before(new Date());
+    }
+
+    // AccessToken 생성
+    public String generateAccessToken(User user) {
+        return generateToken(user.getEmail(), TOKEN_VALIDATION_SECOND);
+    }
+
+    // RefreshToken 생성
+    public String generateRefreshToken(User user) {
+        return generateToken(user.getEmail(), REFRESH_TOKEN_VALIDATION_SECOND);
+    }
+
+    // Token 생성 메소드
+    public String generateToken(String email, long expireSecond) {
+
+        Claims claims = Jwts.claims();
+        claims.put("email", email);
+
+        String token = Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expireSecond))
+                .signWith(getSigningKey(SECRET_KEY), SignatureAlgorithm.HS256)
+                .compact();
+
+        return token;
+    }
+
+    // 유효한 토큰인지 확인
+    public Boolean validateToken(String token, User user) {
+        String email = getEmail(token);
+
+        return (email.equals(user.getEmail()) && !isTokenExpired(token));
+    }
+
+}
