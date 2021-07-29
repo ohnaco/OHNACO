50d34535e022bc70c722caadfc66d9478826ff51
multicampus <kny_1011@naver.com>
2021-07-30 02:19:15 +0900

    S05P12A101-154 - [design] : LeftNavBar에 ProfileImage 컴포넌트 추가 /FE
    사용자명, 알림 아이콘도 추가됨

--- frontend/src/components/common/LeftNavBar.vue
+++ frontend/src/components/common/LeftNavBar.vue
@@ -14,13 +14,35 @@
           <img src="@/assets/images/logo-txt.svg" />
         </v-row>
       </v-container>
+      <v-container>
+        <v-row>
+          <v-col align="center"><profile-image></profile-image></v-col>
+        </v-row>
+        <v-row>
+          <v-col align="center"
+            ><div class="text-h6">
+              {{ username}}<v-icon medium color="white" class="ml-2">mdi-bell</v-icon>
+            </div></v-col
+          >
+        </v-row>
+      </v-container>
     </v-navigation-drawer>
   </v-card>
 </template>
 
 <script>
+import ProfileImage from "./ProfileImage.vue";
+
 export default {
   name: "LeftNavBar",
+  components: {
+    ProfileImage,
+  },
+  data() {
+    return {
+      username: "코린이",
+    };
+  },
 };
 </script>
 
