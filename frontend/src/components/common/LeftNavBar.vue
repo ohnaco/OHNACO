0c733606957dc83add6bed58e3b7e187a48c382e
multicampus <kny_1011@naver.com>
2021-07-30 02:14:18 +0900

    S05P12A101-153 - [design] : LeftNavBar의 상단 로고 /FE

---
+++ frontend/src/components/common/LeftNavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card class="mx-auto">
+    <v-navigation-drawer
+      height="100vh"
+      width="100%"
+      class="blue-grey base pt-3 mx-auto"
+      permanent
+    >
+      <v-container>
+        <v-row justify="center">
+          <router-link to="/"
+            ><img src="@/assets/images/logo-img.svg" class="mr-1"
+          /></router-link>
+          <img src="@/assets/images/logo-txt.svg" />
+        </v-row>
+      </v-container>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "LeftNavBar",
+};
+</script>
+
+<style lang="css" scoped>
+* {
+  color: white;
+}
+</style>
