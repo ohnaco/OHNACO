079ef35909390c775b76140aa25bda2aa93e1bc0
고노윤 <kny_1011@naver.com>
2021-08-05 14:25:45 +0900

    S05P13A101-10 - [design] : Daily Commit 컴포넌트 생성 /FE
    새로고침 클릭 시 호출되는 함수는 임의로 toggle 형태로 구현.
    
    Resolves S05P13A101-10

---
+++ frontend/src/components/todo/DailyCommit.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="d-flex align-center ml-3">
+    <img src="@/assets/images/daily-commit-no.svg" v-if="!todayCommit" />
+    <img src="@/assets/images/daily-commit-ok.svg" v-if="todayCommit" />
+    <v-btn icon><img
+      src="@/assets/images/daily-commit-refresh.svg"
+      @click="commitRefresh"
+    /></v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todayCommit: false,
+    };
+  },
+  methods: {
+    commitRefresh() {
+      this.todayCommit = !this.todayCommit;
+    },
+  },
+};
+</script>
--- frontend/src/views/todo/TodoHome.vue
+++ frontend/src/views/todo/TodoHome.vue
@@ -3,9 +3,13 @@
     <v-row>
       <v-col cols="2"><left-nav-bar></left-nav-bar></v-col>
       <v-col cols="7" class="d-flex flex-column">
-        <div class="d-flex align-center text-h4 ma-5"
-          ><img src="@/assets/images/todo-icon.svg" class="mr-2"/><b>To Do List</b></div
-        >
+        <div class="d-flex align-center text-h4 ma-5">
+          <img src="@/assets/images/todo-icon.svg" class="mr-2" />
+          <b>To Do List</b>
+        </div>
+        <div class="d-flex flex-row align-center ma-5">
+          <h2>{{ date }}</h2>
+          <daily-commit />
         </div>
         <todo-card v-for="todo in todoLists" :key="todo.todoid" :item="todo" />
         <todo-add @finish-create="toggleCreate" v-if="isCreateTodo" />
@@ -25,18 +29,21 @@ import LeftNavBar from "@/components/common/LeftNavBar.vue";
 import TodoAdd from "@/components/todo/TodoAdd.vue";
 import TodoCard from "@/components/todo/TodoCard.vue";
 import { createNamespacedHelpers } from "vuex";
+import DailyCommit from "@/components/todo/DailyCommit.vue";
 const todoListHelper = createNamespacedHelpers("todoStore");
 
 export default {
   data() {
     return {
       isCreateTodo: false,
+      date: "8월 5일 (목)"
     };
   },
   components: {
     LeftNavBar,
     TodoCard,
     TodoAdd,
+    DailyCommit,
   },
   computed: {
     ...todoListHelper.mapState(["todoLists"]),
