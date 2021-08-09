246aadd886b197ceca7749b488207fcbc4b2ba4b
고노윤 <kny_1011@naver.com>
2021-08-09 10:48:12 +0900

    S05P13A101-70 - [feat] : FCM 환경설정 및 알림 추가 /FE
    Serve와의 연동은 추후 예정

---
+++ frontend/public/firebase-messaging-sw.js
@@ -0,0 +1,31 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts("https://www.gstatic.com/firebasejs/8.9.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.9.0/firebase-messaging.js");
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+  apiKey: "AIzaSyAjoO1hK9KSuV3oEsXBjxf0BdSoFwJO6HY",
+  projectId: "ohnaco-notification",
+  messagingSenderId: "1097451483774",
+  appId: "1:1097451483774:web:03068b7284ab4b03ea5e2f",
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+// 백그라운드 상태에서 받은 알림 처리
+messaging.setBackgroundMessageHandler((payload) => {
+  console.log("[firebase-messaging-sw.js] Received background message ", payload);
+  // Customize notification here
+  const notificationTitle = "✅ Daily Commit 알림";
+  const notificationOptions = {
+    body: "아직 잔디를 심지 않으셨네요!",
+    icon: "/firebase-logo.png",
+  };
+
+  return self.registration.showNotification(notificationTitle, notificationOptions);
+});
