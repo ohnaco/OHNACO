5245567e914d585ffc8db3dec590b5e6cb6fb6fa
양다연 <kite324@naver.com>
2021-08-12 19:58:37 +0900

    S05P13A101-68 - [feat] : 마이페이지 infinity scroll 구현/FE

--- frontend/src/views/mypage/MyAnswers.vue
+++ frontend/src/views/mypage/MyAnswers.vue
@@ -6,9 +6,18 @@
         :commit="mypageInfo.commit"
       />
     </div>
-    <v-container class="mt-5" style="width: 70%">
-      내 답변 ({{ mypageInfo.answerCount }})
+    <v-container style="width: 60%">
+      <div class="mt-10 mb-3 d-flex justify-space-between">
+        내 답변 ({{ mypageInfo.answerCount }})
+        <button @click="goback"><img src="@/assets/images/mypage-back-btn.svg" alt="back-btn"></button>
+      </div>
       <v-divider></v-divider>
+      <QuestionCard
+        v-for="answer in myAnswers"
+        :key="answer.answerid"
+        :item="answer"
+        @click="goDetail(answer)"
+      />
         <infinite-loading @infinite="infiniteHandler" spinner="circles"></infinite-loading>
       <v-divider></v-divider>
     </v-container>
@@ -17,6 +26,7 @@
 
 <script>
 import MyPageProfile from "@/components/mypage/MyPageProfile.vue";
+import QuestionCard from "@/components/devtalk/QuestionCard.vue";
 
 import InfiniteLoading from 'vue-infinite-loading';
 import MyPage from "@/api/MyPage";
@@ -28,11 +38,12 @@ export default {
   name: 'MyPage',
   components: {
     MyPageProfile,
-    InfiniteLoading
+    QuestionCard,
+    InfiniteLoading,
   },
   data: function () {
     return {
-      pagenum: 1,
+      pageno: 1,
       myAnswers: []
     }
   },
@@ -44,14 +55,23 @@ export default {
   },
   methods: {
     ...mypageHelper.mapActions(['getMyPage']),
+    goback: function () {
+      this.$router.push({ name: 'MyPage' })
+    },
+    goDetail: function (answer) {
+      this.$router.push({
+        name: "QuestionDetail",
+        params:{ id: answer.questionid },
+      });
+    },
     infiniteHandler($state) {
       MyPage.requestMyAnswers(
-        this.pagenum,
-        (res) => {
+        this.pageno,
+        ({data}) => {
           setTimeout(() => {
-            if (res.data.length) {
-              this.pagenum += 1;
-              this.myAnswers = this.myAnswers.concat(res.data)
+            if (data.answer.length) {
+              this.pageno += 1;
+              this.myAnswers.push(...data.answer)
               $state.loaded();
             } else {
               $state.complete();
