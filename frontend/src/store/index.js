0af5248fa17b11d417a8af5375666984d65d4437
고노윤 <kny_1011@naver.com>
2021-08-12 11:15:06 +0900

    [fix] : 충돌 발생한 store의 index.js 수정 후 누락된 store 추가 /FE

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,12 +2,10 @@ import Vue from "vue";
 import Vuex from "vuex";
 import todoStore from "./modules/todoStore";
 import userStore from "./modules/userStore";
-<<<<<<< HEAD
 import devTalkStore from "./modules/devTalkStore";
-=======
 import techStore from "./modules/techStore";
 import statisticsStore from "./modules/statisticsStore";
->>>>>>> 59216e3935d0218dbe22bb773b8396c07c3143e8
+import mypageStore from "./modules/mypageStore";
 
 Vue.use(Vuex);
 
@@ -15,12 +13,10 @@ const store = new Vuex.Store({
   modules: {
     todoStore: todoStore,
     userStore: userStore,
-<<<<<<< HEAD
     devTalkStore: devTalkStore,
-=======
     techStore: techStore,
     statisticsStore: statisticsStore,
->>>>>>> 59216e3935d0218dbe22bb773b8396c07c3143e8
+    mypageStore: mypageStore,
   },
 });
 
