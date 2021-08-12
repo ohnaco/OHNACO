fa7d88b939526d50ad37ad9a4acc174f9ed1ec77
김현홍 <kimho1995@naver.com>
2021-08-12 21:28:22 +0900

    [fix] : 통계 null 수정 /BE
    ec2에서 동작을 안해서 변경

--- backend/src/main/java/com/prossafy101/ohnaco/repository/StatisticsRepository.java
+++ backend/src/main/java/com/prossafy101/ohnaco/repository/StatisticsRepository.java
@@ -2,16 +2,14 @@ package com.prossafy101.ohnaco.repository;
 
 import com.prossafy101.ohnaco.entity.statistics.StatisticsCategoryDto;
 import com.prossafy101.ohnaco.entity.statistics.StatisticsPositionDto;
-import com.prossafy101.ohnaco.entity.statistics.StatisticsTotalDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @Mapper
 public interface StatisticsRepository {
-    Optional<StatisticsTotalDto> getTotalTime(Map<String, Object> map);
+    Long getTotalTime(Map<String, Object> map);
     List<StatisticsCategoryDto> getCategoryTime(Map<String, Object> map);
     List<StatisticsPositionDto> getPositionTime(Map<String, Object> map);
     List<StatisticsPositionDto> getEntireCategoryTime(Map<String, Object> map);
