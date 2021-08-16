91f4e5f2f0c59d273155d2e0197d2ed135c07c33
양다연 <kite324@naver.com>
2021-08-17 01:47:49 +0900

    S05P13A101 - [design] : 프로필 이미지 수정 /FE

--- frontend/src/views/mypage/ChangeInfo.vue
+++ frontend/src/views/mypage/ChangeInfo.vue
@@ -238,7 +238,7 @@ export default {
               console.log(err)
             } else {
               console.log(data)
-              this.image = "https://ohnaco.s3.ap-northeast-2.amazonaws.com/" + this.email + ".jpg"
+              this.image = "https://ohnaco.s3.ap-northeast-2.amazonaws.com/" + this.email + ".jpg?test="+Math.random().toString(36).substring(2, 11);
               this.updateProfile()
             }
           });
