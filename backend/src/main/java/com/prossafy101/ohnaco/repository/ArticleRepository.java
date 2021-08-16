df334cb7f7e27ebf047c93fdab8e6974433c6a67
김현홍 <kimho1995@naver.com>
2021-08-16 15:23:26 +0000

    [fix]: article query문 수정 /BE

--- backend/src/main/java/com/prossafy101/ohnaco/repository/ArticleRepository.java
+++ backend/src/main/java/com/prossafy101/ohnaco/repository/ArticleRepository.java
@@ -17,7 +17,7 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     Page<Article> findAllByTitleOrContentContaining(String title, String content,Pageable pageable);
 
-    @Query(value = "select a.* from `article` a, `subscribe` s where a.blogid in (select c.blogid from `subscribe` c where c.userid=:userid) ", nativeQuery = true)
+    @Query(value = "select a.* from `article` a where a.blogid in (select c.blogid from `subscribe` c where c.userid=:userid) ", nativeQuery = true)
     Page<Article> findArticles(@Param("userid")String userid, Pageable pageable);
 
     Article findArticleByLink(String link);
