8e65c480d9a3111426f1b7120a77dac8ca2e6726
고노윤 <kny_1011@naver.com>
2021-08-17 02:26:00 +0900

    [fix] : firebase.initializeApp() 에러 수정 /FE

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -4,16 +4,16 @@
 importScripts("https://www.gstatic.com/firebasejs/8.9.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.9.0/firebase-messaging.js");
 
-if (firebase.messaging().isSupported()) {
-  // Initialize the Firebase app in the service worker by passing in the
-  // messagingSenderId.
-  firebase.initializeApp({
-    apiKey: "AIzaSyAjoO1hK9KSuV3oEsXBjxf0BdSoFwJO6HY",
-    projectId: "ohnaco-notification",
-    messagingSenderId: "1097451483774",
-    appId: "1:1097451483774:web:03068b7284ab4b03ea5e2f",
-  });
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+  apiKey: "AIzaSyAjoO1hK9KSuV3oEsXBjxf0BdSoFwJO6HY",
+  projectId: "ohnaco-notification",
+  messagingSenderId: "1097451483774",
+  appId: "1:1097451483774:web:03068b7284ab4b03ea5e2f",
+});
 
+if (firebase.messaging.isSupported()) {
   // Retrieve an instance of Firebase Messaging so that it can handle background
   // messages.
   const messaging = firebase.messaging();
