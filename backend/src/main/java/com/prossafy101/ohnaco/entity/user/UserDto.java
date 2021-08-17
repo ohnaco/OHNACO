2c497f830940ecc5f1dd5894fc27efc30acd586c
김현홍 <kimho1995@naver.com>
2021-08-17 10:47:37 +0000

    [fix]: userDto 생성자 추가 /BE

--- backend/src/main/java/com/prossafy101/ohnaco/entity/user/UserDto.java
+++ backend/src/main/java/com/prossafy101/ohnaco/entity/user/UserDto.java
@@ -1,14 +1,18 @@
 package com.prossafy101.ohnaco.entity.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.Serializable;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto implements Serializable {
     private String email;
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
