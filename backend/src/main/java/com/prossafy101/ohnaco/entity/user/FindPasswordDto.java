63b47ef78a583e5ae39275d4cba35199dd624162
김현홍 <kimho1995@naver.com>
2021-08-18 18:11:01 +0000

    [fix]: 비밀번호찾기 오류 수정 /BE

--- backend/src/main/java/com/prossafy101/ohnaco/entity/user/FindPasswordDto.java
+++ backend/src/main/java/com/prossafy101/ohnaco/entity/user/FindPasswordDto.java
@@ -1,9 +1,13 @@
 package com.prossafy101.ohnaco.entity.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class FindPasswordDto {
     private String email;
