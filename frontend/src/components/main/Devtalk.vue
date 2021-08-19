dbca071f011c80cdbf723450b77eeb503a5ddb94
양다연 <kite324@naver.com>
2021-08-20 02:04:28 +0900

    S05P13A101 - [design] : 메인페이지 모바일버전 사진 추가 /FE

--- frontend/src/components/main/Devtalk.vue
+++ frontend/src/components/main/Devtalk.vue
@@ -1,31 +1,61 @@
 <template>
-  <v-carousel 
-    cycle
-    width="380"
-    height="auto"
-    interval="3000"
-    hide-delimiter-background
-    show-arrows-on-hover
-    hide-delimiters
-    >
-    <v-carousel-item
-      v-for="(item,i) in items"
-      :key="i"
-      :src="item.src"
-    ></v-carousel-item>
-  </v-carousel>
+  <v-layout class="justify-content-center">
+    <v-carousel
+      v-show="$vuetify.breakpoint.smAndUp"
+      cycle
+      width="400"
+      height="auto"
+      interval="3000"
+      hide-delimiter-background
+      show-arrows-on-hover
+      hide-delimiters
+      >
+      <v-carousel-item
+        v-for="(item,i) in webs"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
+    </v-carousel>
+    <v-carousel 
+      v-show="$vuetify.breakpoint.xs"
+      cycle
+      height="auto"
+      width="230"
+      interval="3000"
+      hide-delimiter-background
+      show-arrows-on-hover
+      hide-delimiters
+      >
+      <v-carousel-item
+        v-for="(item,i) in mobiles"
+        :key="i"
+        :src="item.src"
+        width="230"
+        height="auto"
+        class="ml-auto mr-auto"
+      ></v-carousel-item>
+    </v-carousel>
+  </v-layout>
 </template>
 
 <script>
   export default {
     data () {
       return {
-        items: [
+        webs: [
           {
             src: require('@/assets/main/devtalk-list.png'),
           },
           {
             src: require('@/assets/main/devtalk-detail.png'),
+          },
+        ],
+        mobiles: [
+          {
+            src: require('@/assets/main/m-devtalk-list.jpg'),
+          },
+          {
+            src: require('@/assets/main/m-devtalk-detail.jpg'),
           },
         ],
       }
--- frontend/src/components/main/ToDo.vue
+++ frontend/src/components/main/ToDo.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div class="d-flex justify-content-center">
     <!-- 웹 -->
-    <v-card style="width: 400; height: auto;" v-show="$vuetify.breakpoint.mdAndUp">
+    <v-card style="width: 400; height: auto;" v-show="$vuetify.breakpoint.smAndUp">
       <v-tabs
         v-model="tab"
         background-color="transparent"
@@ -30,7 +30,7 @@
       </v-tabs-items>
     </v-card>
     <!-- 모바일 -->
-    <!-- <v-card v-show="$vuetify.breakpoint.smAndDown">
+    <v-card width="260" height="auto" v-show="$vuetify.breakpoint.xs">
       <v-tabs
         v-model="tab"
         background-color="transparent"
@@ -52,11 +52,17 @@
           :key="mobile.title"
         >
           <v-card flat>
+            <v-img 
+              width="250"
+              height="auto"
+              class="ml-auto mr-auto"
+              :src="mobile.src"
+            ></v-img>
             <v-card-text>{{ mobile.content }}</v-card-text>
           </v-card>
         </v-tab-item>
       </v-tabs-items>
-    </v-card> -->
+    </v-card>
   </div>
 </template>
 
@@ -69,24 +75,36 @@
           { 
             title: 'Todo생성', 
             src: require('@/assets/main/todo-update.gif'), 
-            content: 'Todo 생성에서는 제공하는 카테고리를 선택 후 내용을 작성합니다. 목표 시간을 설정한 뒤 Todo 생성을 완료하면 재생버튼을 통해 타이머를 실행시킬 수 있습니다. 할일을 완료했다면 정지버튼을 클릭해 주세요.' 
+            content: 'Todo 생성에서는 제공하는 카테고리를 선택 후 내용을 작성합니다. 목표 시간을 설정한 뒤 Todo 생성을 완료하면 재생버튼을 통해 타이머를 실행시킬 수 있습니다. 정지버튼을 눌러 시간을 멈추거나 할일을 완료할 수 있습니다.' 
           },
           { 
             title: 'todo이전', 
             src: require('@/assets/main/todo-past.gif'), 
-            content: '과거의 Todo를 완료하지 못했거나 오늘 날짜에 추가하고 싶다면 해당 날짜의 Todo 리스트에서 플러스 버튼을 클릭해주세요. 오늘 날짜의 할일 목록에 추가됩니다.' 
+            content: '과거의 Todo를 완료하지 못했거나 오늘 날짜에 추가하고 싶다면 과거의 Todo 리스트에서 플러스 버튼을 클릭해주세요. 오늘의 할일 목록에 추가됩니다.' 
           },
           { 
             title: '달력', 
             src: require('@/assets/main/todo-calendar.gif'), 
-            content: '달력 아이콘을 클릭하면 나의 모든 할일 목록을 볼 수 있으며 해당 날짜의 Todo 리스트를 볼 수 있습니다.' 
+            content: '달력 아이콘을 클릭하면 나의 모든 할일 목록을 볼 수 있으며 해당 날짜의 Todo 리스트로 이동이 가능합니다.' 
           },
         ],
-        // mobiles: [
-        //   { title: 'Todo생성', src: require(), content: 'Tab 1 Content' },
-        //   { title: '추가기능', src: require(), content: 'Tab 2 Content' },
-        //   { title: '달력', src: require(), content: 'Tab 3 Content' },
-        // ],
+        mobiles: [
+          { 
+            title: 'Todo생성', 
+            src: require('@/assets/main/m-todo-update.gif'), 
+            content: 'Todo 생성에서는 제공하는 카테고리를 선택 후 내용을 작성합니다. 목표 시간을 설정한 뒤 Todo 생성을 완료하면 재생버튼을 통해 타이머를 실행시킬 수 있습니다. 할일을 완료했다면 정지버튼을 클릭해 주세요.' 
+          },
+          { 
+            title: 'todo이전', 
+            src: require('@/assets/main/m-todo-past.gif'), 
+            content: '과거의 Todo를 완료하지 못했거나 오늘 날짜에 추가하고 싶다면 해당 날짜의 Todo 리스트에서 플러스 버튼을 클릭해주세요. 오늘 날짜의 할일 목록에 추가됩니다.' 
+          },
+          { 
+            title: '달력', 
+            src: require('@/assets/main/m-todo-calendar.gif'), 
+            content: '달력 아이콘을 클릭하면 해당 날짜의 Todo 리스트로 이동할 수 있습니다.' 
+          },
+        ],
       }
     },
   }
