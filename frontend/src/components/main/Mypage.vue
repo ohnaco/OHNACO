227b5619eb6fa3edfc216ac52e5dbb6b9ac90837
양다연 <kite324@naver.com>
2021-08-19 22:45:37 +0900

    S05P13A101 - [design] : 마이페이지 모바일 이미지 사이즈 조절 /FE

--- frontend/src/components/main/Mypage.vue
+++ frontend/src/components/main/Mypage.vue
@@ -1,39 +1,31 @@
 <template>
-<div>
-  <v-carousel
-    v-show="$vuetify.breakpoint.mdAndUp"
-    cycle
-    height="auto"
-    width="400"
-    interval="3000"
-    hide-delimiter-background
-    show-arrows-on-hover
-    hide-delimiters
-    >
-    <v-carousel-item
-      v-for="(item,i) in items"
-      :key="i"
-      :src="item.src"
-    ></v-carousel-item>
-  </v-carousel>
-  <v-carousel
-    v-show="$vuetify.breakpoint.smAndDown"
-    cycle
-    height="auto"
-    width="130"
-    interval="3000"
-    hide-delimiter-background
-    show-arrows-on-hover
-    hide-delimiters
-    >
-    <v-carousel-item
-    >
-    <v-img
-      :src='require("@/assets/main/m-mypage.gif")'
-    ></v-img>
-    </v-carousel-item>
-  </v-carousel>
-</div>
+  <v-layout class="justify-content-center">
+    <v-carousel
+      v-show="$vuetify.breakpoint.smAndUp"
+      cycle
+      height="auto"
+      width="400"
+      interval="3000"
+      hide-delimiter-background
+      show-arrows-on-hover
+      hide-delimiters
+      >
+      <v-carousel-item
+        v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
+    </v-carousel>
+    <v-card
+      v-show="$vuetify.breakpoint.xs"
+      height="auto"
+      width="200"
+      >
+      <v-img
+        :src='require("@/assets/main/m-mypage.gif")'
+      ></v-img>
+    </v-card>
+  </v-layout>
 </template>
 
 <script>
