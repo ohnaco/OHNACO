04c149ef6bbd5c6fc4632a0876be5d114390579b
양다연 <kite324@naver.com>
2021-08-19 20:57:16 +0900

    S05P13A101 - [design] : 메인페이지 디자인 수정/FE

--- frontend/src/components/main/Tech.vue
+++ frontend/src/components/main/Tech.vue
@@ -1,7 +1,11 @@
 <template>
+<div>
   <v-carousel 
+    v-show="$vuetify.breakpoint.mdAndUp"
     cycle
-    height="400"
+    height="auto"
+    width="400"
+    class="pa-0"
     interval="3000"
     hide-delimiter-background
     show-arrows-on-hover
@@ -13,6 +17,23 @@
       :src="item.src"
     ></v-carousel-item>
   </v-carousel>
+  <!-- <v-carousel 
+    v-show="$vuetify.breakpoint.smAndDown"
+    cycle
+    height="auto"
+    width="400"
+    interval="3000"
+    hide-delimiter-background
+    show-arrows-on-hover
+    hide-delimiters
+    >
+    <v-carousel-item
+      v-for="(mobile,i) in mobiles"
+      :key="i"
+      :src="mobile.src"
+    ></v-carousel-item>
+  </v-carousel> -->
+</div>
 </template>
 
 <script>
@@ -21,12 +42,20 @@
       return {
         items: [
           {
-            src: require('@/assets/images/tech-page.png'),
+            src: require('@/assets/main/tech-sub.gif'),
           },
           {
-            src: require('@/assets/images/scrap-page.png'),
+            src: require('@/assets/main/tech-scr.gif'),
           },
         ],
+        // mobiles: [
+        //   {
+        //     src: require('@/assets/main/mypage.png'),
+        //   },
+        //   {
+        //     src: require('@/assets/main/todo.png'),
+        //   },
+        // ],
       }
     },
   }
